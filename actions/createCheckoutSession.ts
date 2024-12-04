8d4145150acb48b27cfd14a093cc3a482ff13a3c
hammadshykh <hammadyounus.508@gmail.com>
2024-12-04 19:05:43 +0500

    update: vercel

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -21,8 +21,8 @@ export async function createCheckoutSession(
  metadata: MetaData
 ) {
  const baseUrl =
-  !process.env.VERCEL_URL == undefined
-   ? `https://${process.env.VERCEL_URL}`
+  process.env.NODE_ENV == "production"
+   ? `${process.env.VERCEL_URL}`
    : process.env.NEXT_PUBLIC_BASE_URL;
 
  console.log(
